8e1ac15e80889e69d8725e049eead9dfcc739dce
Joe <josephfmck@gmail.com>
2020-07-09 15:15:25 -0500

    Setup express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const express = require("express"); 
+
+const app = express();
+
+app.get("/", (request, response) => {
+    response.send("hi there");
+});
+
+app.listen(3000, () => {
+    console.log("listening on port 3000");
+});
