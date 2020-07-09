0da4bfaca8f0f89df3b9a80b1fead6a674d2b78c
Joe <josephfmck@gmail.com>
2020-07-09 14:56:00 -0500

    added notes.txt

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-//npm init -y
-//npm install express nodemon
-
-//In package.json
-// "scripts": {
-//     "dev": "nodemon index.js"
-//   }
-//allows to run scripts
-//*npm run dev       Main way to run script dev
-
 console.log("hi");
