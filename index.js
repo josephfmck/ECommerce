242e25caa76768f669080d7e77cc2bdb99c46bcf
Joe <josephfmck@gmail.com>
2020-07-10 16:11:45 -0500

    hard coded parsed form data

--- index.js
+++ index.js
@@ -16,6 +16,20 @@ app.get("/", (request, response) => {
 });
 
 app.post("/", (request, response) => {
+    //data event when data passed in
+    request.on("data", data => {
+        //data = buffer obj raw info hex vals, converted to utf8, then parsed into arr with split
+        const parsed = data.toString("utf8").split("&");
+        const formData = {};
+        for (let pair of parsed) {
+            //1st el assign to key, 2nd el assign to value
+            const [key, value] = pair.split("=");
+            console.log(key, value);
+            formData[key] = value;
+        }
+        console.log(formData); 
+    });
+
     response.send("Account Created");
 });
 
