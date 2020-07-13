2cab091adce5c5b626b37b893961614c79c64d83
Joe <josephfmck@gmail.com>
2020-07-13 16:21:14 -0500

    replaced hardcoded bodyParser with global bodyParser library

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express"); 
+const bodyParser = require("body-parser");
 
 const app = express();
+
+//BodyParser = Middleware library = does some pre-processing on the "req" and "res" objects
+//Globally apply middleware to all routes, parses for all requests
+app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get("/", (request, response) => {
     response.send(`
@@ -16,34 +21,9 @@ app.get("/", (request, response) => {
 });
 
 
-//Middleware function = does some pre-processing on the "req" and "res" objects
-//next callback executed when middleware done
-const bodyParser = (request, response, next) => {
-    if(request.method === "POST") {
-        //data event when data passed in
-        request.on("data", data => {
-            //data = buffer obj raw info hex vals, converted to utf8, then parsed into arr with split
-            const parsed = data.toString("utf8").split("&");
-            const formData = {};
-            for (let pair of parsed) {
-                //1st el assign to key, 2nd el assign to value
-                const [key, value] = pair.split("=");
-                console.log(key, value);
-                formData[key] = value;
-            }
-            //console.log(formData); 
-            request.body = formData;
-            next();
-        });
-    } else {
-        next(); //once all done with processing
-    }
-};
-
-//when we post request, 1st run middleware bodyParser, then pass callback to next()
-app.post("/", bodyParser, (request, response) => {
+app.post("/", (request, response) => {
     //console.log(request); //method: POST
-    //console.log(request.body);
+    console.log(request.body);
 
     response.send("Account Created");
 });
