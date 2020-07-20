2ff62a776e26e74a193ee34030692e197d680aa6
Joe <josephfmck@gmail.com>
2020-07-20 15:55:00 -0500

    setup cookie session, fetched session using user ID

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express"); 
 const bodyParser = require("body-parser");
+const cookieSession = require("cookie-session");
 const usersRepo = require("./repositories/users");
 
 const app = express();
@@ -8,9 +9,16 @@ const app = express();
 //Globally apply middleware to all routes, parses for all requests
 app.use(bodyParser.urlencoded({ extended: true}));
 
+//  Production Grade Authentication with Cookies
+app.use(cookieSession({
+    keys: ["jfkajklfksfklj"]
+})); //pass in with configur obj, keys prop encrypts
+
+
 app.get("/", (request, response) => {
     response.send(`
         <div>
+            Your id is: ${request.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email"/>
                 <input name="password" placeholder="password"/>
@@ -41,12 +49,13 @@ app.post("/", async (request, response) => {
         return response.send("Passwords must match");
     }
 
-    //  Producation Grade Authentication with Cookies
+    //  Production Grade Authentication with Cookies
     //  Create a user in our user repo to represent this person
     const user = await usersRepo.create({ email: email, password: password});
     
     //  Store the id of that user inside the users cookie
-    
+    // req.session prop Added by cookie session
+    request.session.userId = user.id; //obj with info inside maintains session
 
     response.send("Account Created");
 });
