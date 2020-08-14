10b6e5b6b13b74a2b960a3c11f7828ab77654729
Joe <josephfmck@gmail.com>
2020-08-14 16:53:03 -0500

    create productsIndexTemplate to render products on homepage, hook up to products get router

--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,16 @@
 //*Set of route handlers related to products that are exposed to all users of application
 
 const express = require("express");
+const productsRepo = require("../repositories/products");
+const productsIndexTemplate = require("../views/products/index");
 
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    res.send("Products!!");
+    //arr of objs
+    const products = await productsRepo.getAll();
+
+    res.send(productsIndexTemplate({ products: products}));
 });
 
 module.exports = router;
---
+++ views/products/index.js
@@ -0,0 +1,14 @@
+//template that renders out all products
+module.exports = ({ products }) => {
+    const renderedProducts = products.map((product) => {   
+        return `
+            <li>${product.title} - ${product.price}</li>
+        `;
+    }).join("");
+
+    return `
+        <ul>
+            ${renderedProducts}
+        </ul>
+    `;
+};
