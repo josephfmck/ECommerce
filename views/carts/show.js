9827b5b95858507cbf5590ad2fb43dc6fc368d89
Joe <josephfmck@gmail.com>
2020-08-20 16:29:07 -0500

    add logic to calculate and dispe

--- views/carts/show.js
+++ views/carts/show.js
@@ -1,6 +1,18 @@
 const layout = require('../layout');
 
 module.exports = ({ items }) => {
+
+  //SIMPLE WAY
+  //let totalPrice = 0;
+  // for(let item of items) {
+  //   totalPrice += item.quantity * item.product.price;
+  // }
+
+  //REDUCE WAY
+  const totalPrice = items.reduce((accum, item) => {
+    return accum + item.quantity * item.product.price;
+  }, 0); //0 is starting val
+
   const renderedItems = items
     .map(item => {
       return `
@@ -42,7 +54,7 @@ module.exports = ({ items }) => {
               <div class="message-header">
                 Total
               </div>
-              <h1 class="title">$</h1>
+              <h1 class="title">$${totalPrice}</h1>
               <button class="button is-primary">Buy</button>
             </div>
           </div>
